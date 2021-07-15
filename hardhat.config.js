ce43d152c31667183e96787abef3f497ba2d9f73
Andrew Parker <andrewdangerparker@gmail.com>
2021-07-15 16:53:26 +1000

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,30 +4,12 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "ROPSTEN_ALCHEMY_KEY" in env
 const ROPSTEN_ALCHEMY_API_KEY = process.env.ROPSTEN_ALCHEMY_KEY;
-
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "MAINNET_ALCHEMY_KEY" in env
 const MAINNET_ALCHEMY_API_KEY = process.env.MAINNET_ALCHEMY_KEY;
-
-// Replace ROPSTEN_PRIVATE_KEY with your Ropsten account private key in .env
-// Replace MAINNET_PRIVATE_KEY with your Mainnet account private key in .env
-// (They will be the same if you use the same address on Ropsten and Mainnet)
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
 const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
-
 const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY;
-
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
-
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
 
@@ -36,12 +18,6 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
 
@@ -61,8 +37,6 @@ module.exports = {
 
   },
   etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
     apiKey: ETHERSCAN_API_KEY
 
   }
